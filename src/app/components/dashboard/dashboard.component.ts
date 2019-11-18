fb6cbad07d7ce72aebdfb3ebd8c8b2cbf2fb3897
Korey Mitchell <koreymitchell@Koreys-MacBook-Pro.local>
2019-11-18 14:03:11 -0500

    updates

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, NgZone } from '@angular/core';
 import { AuthService } from "../../shared/services/auth.service";
 import { Router } from "@angular/router";
-
+declare var jquery: any;
+declare var $: any;
 
 @Component({
   selector: 'app-dashboard',
@@ -16,6 +17,32 @@ export class DashboardComponent implements OnInit {
     public ngZone: NgZone
   ) { }
 
-  ngOnInit() { }
+  ngOnInit() { $("#form").toggle(); }
+
+  toggleClassUser() {
+    $("#userinfo").toggle();
+    $("#form").toggle();
+  }
+
+  toggleClassPost() {
+    $("#form").toggle();
+    $("#userinfo").toggle();
+  }
+  
+  addPost(){
+    // alert("Yes");
+    // alert($('#postinput').val());
+    $('#results').append('<div style="width: 50vw; margin-left:auto; margin-right:auto; border: 2px solid red; border-radius: 5px; margin-top: 10px;">'+ $('#postinput').val());
+
+
+    // $('#results').append(
+    //   $('<div>', {
+    //     type: 'text',
+    //     val: $('#postinput').text()
+    //   })
+    // );
+  }
 
 }
+
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,8 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    authDomain: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    databaseURL: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    projectId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    storageBucket: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    messagingSenderId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+  firebase : {
+  
   }
 };
 
